be2204ce2b39f0f890d7e180123977c8e008bd70
jeffersondrs <jeffersonduarte@id.uff.br>
2022-10-05 19:27:53 -0300

    updating

--- src/components/videocomponent/Video.component.jsx
+++ src/components/videocomponent/Video.component.jsx
@@ -3,7 +3,7 @@ import "./vide.style.scss";
 
 export default function Video() {
   return (
-    <div className="body-container w-full bg-white/30 flex text-white flex-wrap items-center h-full justify-center p-2 my-12">
+    <div className="body-container w-full bg-white/10 flex text-white flex-wrap items-center h-full justify-center p-2 my-12">
       <video
         autoPlay
         muted
@@ -24,7 +24,7 @@ export default function Video() {
             contribuir em projetos e contribuir com a empresa em que eu estiver.
           </p>
         </div>
-        <div className="flex  w-full h-full items-center flex-col justify-center p-2">
+        <div className="flex  w-full h-full items-center flex-col justify-center p-2 bg-black">
           <div className="flex flex-row flex-wrap justify-center">
             <SiJavascript className="bg-black text-yellow-400 xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10" />
             <SiReact className="text-sky-400  react-icon xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10" />
@@ -60,7 +60,7 @@ export default function Video() {
               className="xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
             />
           </div>
-          <div className=" bg-black text-center">
+          <div className="text-center">
             <p>tecnologias que utilizo no meu dia a dia</p>
           </div>
         </div>
