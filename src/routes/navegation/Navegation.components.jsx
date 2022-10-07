255b6751146819e6f8e602cf782219bf475e4aef
jeffersondrs <jeffersonduarte@id.uff.br>
2022-10-07 19:02:20 -0300

    updating

--- src/routes/navegation/Navegation.components.jsx
+++ src/routes/navegation/Navegation.components.jsx
@@ -5,7 +5,7 @@ import Contato from "./Contact.radix";
 export default function Navegation() {
   return (
     <>
-    <div className="bg-black w-full flex flex-row items-center justify-center">
+    <div className="bg-black w-full flex flex-row items-center justify-center p-2">
     <Contato />
     </div>
       <Outlet />
