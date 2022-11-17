f6b31d53816a8421eaa60a756231d8b70138da77
jeffersondrs <jeffersonduarte@id.uff.br>
2022-11-16 23:22:52 -0300

    updating

--- src/components/videocomponent/Video.component.jsx
+++ src/components/videocomponent/Video.component.jsx
@@ -14,13 +14,13 @@ export default function Video() {
       </div>
       <div className="flex w-96 h-full items-center flex-col m-3">
         <div className="flex flex-row flex-wrap justify-evenly items-center">
-          <SiJavascript className="bg-black text-yellow-400 xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10" />
-          <SiReact className="text-sky-400  react-icon xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10" />
-          <SiTailwindcss className="text-sky-400 xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10" />
+          <SiJavascript className="bg-black my-2 text-yellow-400 xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10" />
+          <SiReact className="text-sky-400 my-2 react-icon xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10" />
+          <SiTailwindcss className="text-sky-400 my-2 xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10" />
 
           <img
             src="https://cdn.worldvectorlogo.com/logos/vitejs.svg"
-            className="xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
+            className="my-2 xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
           />
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg"
@@ -28,24 +28,24 @@ export default function Video() {
           />
           <img
             src="https://www.vectorlogo.zone/logos/mysql/mysql-official.svg"
-            className="xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
+            className="my-2 xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
           />
 
           <img
             src="https://www.vectorlogo.zone/logos/mongodb/mongodb-ar21.svg"
-            className="xl:w-28 xl:h-20 lg:w-24 lg:h-16 md:w-20 md:h-12 sm:w-14 sm:h-10 w-16 h-10 "
+            className="my-2 xl:w-28 xl:h-20 lg:w-24 lg:h-16 md:w-20 md:h-12 sm:w-14 sm:h-10 w-16 h-10 "
           />
           <img
             src="https://www.svgrepo.com/show/303205/html-5-logo.svg"
-            className="xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
+            className="my-2 xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
           />
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
-            className="xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
+            className="my-2 xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
           />
           <img
             src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
-            className="xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
+            className="my-2 xl:w-20 xl:h-20 lg:w-16 lg:h-16 md:w-12 md:h-12 sm:w-10 sm:h-10 w-10 h-10 "
           />
         </div>
         <div className="text-center mt-5">
